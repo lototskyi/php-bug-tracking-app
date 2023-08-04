b1190afbc6103e415be11d156e4a87fb38bf70e9
alex <o.lototskyi@gmail.com>
2023-08-04 17:54:31 +0300

    created logger using PSR-3 Logger Interface

--- public/index.php
+++ public/index.php
@@ -4,17 +4,9 @@ declare(strict_types = 1);
 use App\Helpers\Config;
 
 require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/Src/Exception/exception.php';
 
-require __DIR__ . '/Src/Exception/exception.php';
+$logger = new \App\Logger\Logger();
+$logger->log(\App\Logger\LogLevel::EMERGENCY, 'There is an emergency!', ['exception' => 'exception occurred']);
 
-$application = new \App\Helpers\App();
-
-new mysqli('dsdd', 'dsds', 'dsds', 'ddd');
-exit;
-
-echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
-echo $application->getLogPath() . PHP_EOL;
-echo $application->getEnvironment() . PHP_EOL;
-echo $application->isDebug() . PHP_EOL;
-var_dump( $application->isRunningFromConsole());
-
+$logger->info('User account created successfully!', ['id' => 5]);
