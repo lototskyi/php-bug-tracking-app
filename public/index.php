88d68b00f6018d8ee9be3b06f29e35964fd7934e
alex <o.lototskyi@gmail.com>
2023-08-04 10:13:32 +0300

    added App helper

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@ declare(strict_types = 1);
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$config = \App\Helpers\Config::get('app');
+$application = new \App\Helpers\App();
 
-var_dump($config);
+echo $application->getServerTime()->format('Y-m-d H:i:s') . PHP_EOL;
+echo $application->getLogPath() . PHP_EOL;
+echo $application->getEnvironment() . PHP_EOL;
+echo $application->isDebug() . PHP_EOL;
+var_dump( $application->isRunningFromConsole());
+
